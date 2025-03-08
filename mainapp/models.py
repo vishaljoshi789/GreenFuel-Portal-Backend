67fb27a9e5351279bc7d0af4b1423d08b0f3b5ab
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-03-08 15:31:49 +0530

    changed user model

--- mainapp/models.py
+++ mainapp/models.py
@@ -25,6 +25,15 @@ class User(AbstractUser):
     dob = models.DateField(null=True, blank=True)
     employee_code = models.CharField(max_length=255, null=True, blank=True)
     designation = models.ForeignKey(Designation, on_delete=models.CASCADE, null=True, blank=True)
+    business_unit = models.ForeignKey(BusinessUnit, on_delete=models.CASCADE, null=True, blank=True)
+    department = models.CharField(max_length=255, null=True, blank=True)
+    contact = models.CharField(max_length=255, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    status = models.BooleanField(default=True)
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
