7f48c916165ea893edb94922fa364c46d01af05a
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-03-10 17:01:22 +0530

    added models to admin page

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User, BusinessUnit, Department, Designation, ApprovalRequestForm, ApprovalRequestItem
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(BusinessUnit)
+admin.site.register(Department)
+admin.site.register(Designation)
+admin.site.register(ApprovalRequestForm)
+admin.site.register(ApprovalRequestItem)
+
