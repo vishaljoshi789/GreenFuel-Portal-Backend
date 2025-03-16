14d62b10feaeb7af280b80510c40a63b5e500fd6
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-03-16 20:09:55 +0530

    added Chat model

--- mainapp/models.py
+++ mainapp/models.py
@@ -139,6 +139,23 @@ class ApprovalRequestItem(models.Model):
 
     def __str__(self):
         return str(self.user)
+    
+class ChatRoom(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
+
+    def __str__(self):
+        return str(self.user1)
+    
+class Chat(models.Model):
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.sender)
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
