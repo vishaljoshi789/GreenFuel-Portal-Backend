d18acedf7280dcb74dce55e34b2e1a6fd5942ff4
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-03-31 10:32:44 +0530

    solved cors error

--- greenfuel/settings.py
+++ greenfuel/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["192.168.1.3", "127.0.0.1", "localhost"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -81,11 +80,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://192.168.1.3:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:8080"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
