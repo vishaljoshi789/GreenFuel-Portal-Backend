74a29fac1c0c4f97270b0dc7387083a9e572a95f
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-07-30 00:46:48 +0530

    solved bug

--- mainapp/models.py
+++ mainapp/models.py
@@ -12,9 +12,11 @@ def approval_pdf_upload_path(instance, filename):
     return f'pdfs/{user_email}/{filename}'
 
 def attachment_upload_path(instance, filename):
-    if instance.form is None or instance.form.user is None:
-        return f'attachments/unknown/{filename}'
-    return f'attachments/{instance.form.user.email}/{filename}'
+    try:
+        user_email = instance.user.email
+    except Exception:
+        user_email = "unknown"
+    return f'attachments/{user_email}/{filename}'
 
 class BusinessUnit(models.Model):
     name = models.CharField(max_length=300)
@@ -107,7 +109,7 @@ class ApprovalRequestForm(models.Model):
     payback_period = models.CharField(max_length=255, null=True, blank=True)
     document_enclosed_summary = models.TextField(null=True, blank=True)
     current_status = models.CharField(max_length=255, default='Pending')
-    # pdf = models.FileField(upload_to=approval_pdf_upload_path, null=True, blank=True, default=None)
+    pdf = models.FileField(upload_to=approval_pdf_upload_path, null=True, blank=True, default=None)
 
     def budget_id(self):
         return str(self.id + 9999999)
